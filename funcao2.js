902fdce3053a739776ad7f46027e975e56ffb558
Rodrigo Costa <rfcosta85@gmail.com>
2020-12-01 20:48:46 -0300

    Aulas função 1 e 2 e aulas var em loop

---
+++ funcao2.js
@@ -0,0 +1,29 @@
+// Armazenando uma função em uma variável
+
+const imprimirSoma = function (a, b)
+{
+
+    console.log(a + b)
+
+}
+
+imprimirSoma(2, 3)
+
+
+// Armazenando uma função arrow em uma variável
+
+const soma = (a, b) => 
+{
+
+    return a + b
+
+}
+
+console.log(soma(2, 3))
+
+// Arrow Function implícito
+
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(3, 2))
