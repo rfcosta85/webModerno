67a3d55c7e29d75cc8c81217c1ee0bc19e205011
Rodrigo Costa <rfcosta85@gmail.com>
2020-12-15 17:22:48 -0300

    Ex 03 Dividendo

---
+++ 05-funcoes/exercicios/dividendo.js
@@ -0,0 +1,13 @@
+function dividendAndRestOfTheDivision(number1, number2)
+{
+
+    let division = number1 / number2
+
+    let rest = number1 % number2
+
+    console.log(`${number1} / ${number2} = ${division}`)
+    console.log(`O resto da divisão de ${number1} por ${number2} é ${rest}`)
+
+}
+
+dividendAndRestOfTheDivision(6, 2)
