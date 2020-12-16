45b482765411ee116c3f191d5ac547879155e273
Rodrigo Costa <rfcosta85@gmail.com>
2020-12-16 11:45:25 -0300

    ex 06 Aplicação

---
+++ 05-funcoes/exercicios/06-aplicacao.js
@@ -0,0 +1,23 @@
+function simpleInterest(inicialAmount, interest, applicationTime)
+{
+
+    let factor = interest/100      
+
+    return applicationTime * (inicialAmount * factor) + inicialAmount
+
+}
+
+function compoundInterest(inicialAmount, interest, applicationTime)
+{
+
+    factor = interest/100
+
+    return inicialAmount * (1 + factor) ** applicationTime
+
+}
+
+let result = simpleInterest(300000, 6, 1)
+let compoundResult = compoundInterest(300000, 6, 3)
+
+console.log(`O rendimento em juros simples será de: ${result.toLocaleString('pt-br', {style:'currency' ,currency: 'BRL'})}`)
+console.log(`O rendimento em juros compostos será de: ${compoundResult.toLocaleString('pt-br', {style:'currency' ,currency: 'BRL'})}`)
