633e21791605e0f9477cf9876525ac71c6054bac
Rodrigo Costa <rfcosta85@gmail.com>
2020-12-17 11:40:08 -0300

    ex 10 multiplo de 3

---
+++ 05-funcoes/exercicios/10-multiploDe3.js
@@ -0,0 +1,22 @@
+function isMultiple(number)
+{
+
+    let multipleOfThree = number 
+
+    if(multipleOfThree % 3 == 0)
+    {
+
+        return true
+
+    }else
+    {
+
+        return false 
+
+    }
+
+}
+
+let result = isMultiple(3)
+
+console.log(result)
