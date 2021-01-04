90eb7d5c665d822e1110d21d1e7115ce1e97123a
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-04 12:57:29 -0300

    Ex25 - Exibir  Números de 1 a 50 e Ex26 - Exibir números pares de 1 até 100

---
+++ 05-funcoes/exercicios/25-contando.js
@@ -0,0 +1,13 @@
+function counting()
+{
+
+    for(index = 1; index <= 50; index++)
+    {
+
+        console.log(index)
+
+    }
+
+}
+
+console.log(counting())
---
+++ 05-funcoes/exercicios/26-pares.js
@@ -0,0 +1,18 @@
+function par()
+{
+
+    for(index = 1; index <= 100; index++)
+    {
+
+        if(index % 2 == 0)
+        {
+
+            console.log(index)
+
+        }
+
+    }
+
+}
+
+console.log(par())
