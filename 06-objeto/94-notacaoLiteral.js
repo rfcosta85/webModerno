709768f0a4b6fceb8d69116453a5925c27839d08
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-06 15:59:49 -0300

    Aula 94- Notação Literal

---
+++ 06-objeto/94-notacaoLiteral.js
@@ -0,0 +1,15 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj = {a, b , c}
+
+console.log(obj)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj2 = {}
+obj2[nomeAttr] = valorAttr
+
+console.log(obj2)
