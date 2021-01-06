dc8d50724b5f12027306e589ab7765840ef0eb52
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-06 11:45:40 -0300

    Aula 90- Introdução à OO

---
+++ 06-objeto/oo.js
@@ -0,0 +1,20 @@
+// Código não executável!!!
+
+processamento(valor1, valor2, valor3)  // Paradigma procedural 
+
+// OO
+
+objeto = 
+{
+    valor1,
+    valor2,
+    valor3,
+    processamento()
+    {
+        //...
+    }
+
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
