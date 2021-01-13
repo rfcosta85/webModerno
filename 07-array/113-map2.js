a98452a22298211ca2c6204ef7a528bfa146e8a5
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-13 12:20:36 -0300

    aula 114- map 03

---
+++ 07-array/113-map2.js
@@ -0,0 +1,22 @@
+const carrinho = [
+
+    '{"nome": "Borracha", "preço": 3.45}',
+    '{"nome":"Caderno", "preço": 13.90}',
+    '{"nome": "Kit de Lapis", "preço": 41.22}',
+    '{"nome": "Caneta", "preço": 7.50}'
+
+]
+
+// Retornar um array apenas com os preços
+
+// Precisamos chamar um json.parse para transformar as strings em um objeto
+
+const paraObjeto = json => JSON.parse(json)
+
+const precos = precosDoProdutos => precosDoProdutos.preço
+
+const paraDinheiro = e => `${parseFloat(e).toLocaleString('pt-BR', {style:'currency', currency: 'BRL'})}`
+
+const resultado = carrinho.map(paraObjeto).map(precos).map(paraDinheiro)
+
+console.log(resultado)
