39102b8940b3675560258bb5a64e15a6e7aa9738
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-18 16:12:32 -0300

    ex06- É igual

---
+++ 07-array/exerciciosArray/05-eIgual.js
@@ -0,0 +1,18 @@
+function isEqual(firstNumber, secondNumber)
+{
+
+    if(firstNumber === secondNumber)
+    {
+
+        return true
+
+    }else
+    {
+
+        return false
+
+    }
+
+}
+
+console.log(isEqual(3,12))
