22b1ada5af77af6f26bc8c23f42d57822804110b
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-18 16:19:24 -0300

    ex07- inverso

---
+++ 07-array/exerciciosArray/06-inverso.js
@@ -0,0 +1,17 @@
+function inverse(boolean, number)
+{
+
+    if(boolean == true)
+    {
+
+        return `${false} e ${-number}`
+
+    }else 
+    {
+
+        return `${true} e ${-number}`
+    }
+
+}
+
+console.log(inverse(false, 245))
