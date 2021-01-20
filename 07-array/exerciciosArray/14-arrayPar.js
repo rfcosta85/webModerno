aad2f77b30d23624d6b034aa5df4446f15d2f884
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-20 11:57:14 -0300

    exercicio 14

---
+++ 07-array/exerciciosArray/14-arrayPar.js
@@ -0,0 +1,23 @@
+const array = [1, 3, 5, 6, 12, 30, 27]
+
+function pairArray(array)
+{
+
+    let newArray = []
+
+    for(let i = 0; i < array.length; i++)
+    {
+
+        if(array[i] % 2 == 0 || i % 2 == 0)
+        {
+
+            newArray.push(array[i])
+
+        }
+
+    }
+
+    return newArray
+}
+
+console.log(pairArray(array))
