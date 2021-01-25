77ce9532b1c107c33c9bda720eedad47316ac02e
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-25 16:47:14 -0300

    Aula 131- Resolvendo Desafio - Menor salário mulheres chinesas

--- 09-node/130-funcionarios/130-funcionarios.js
+++ 09-node/130-funcionarios/130-funcionarios.js
@@ -2,9 +2,24 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 
 const axios = require('axios') // o axios é um cliente http. Ele faz requisições para obter informações de algo que está remoto. Aqui estamos obtendo o arquivo json do link para obter as informações contidas nele
 
+const chineses = chineseWorks => chineseWorks.pais === 'China'
+const mulheres = chineseWorks => chineseWorks.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+
+    return func.salario < funcAtual.salario ? func : funcAtual 
+
+}
+
 axios.get(url).then(response => {
 
     const funcionarios = response.data
-    console.log(funcionarios)
-    
+    //console.log(funcionarios)
+
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+
 }) // Aqui vamos fazer uma requisição em cima da url e vamos obter o conteúdo do arquivo no código
