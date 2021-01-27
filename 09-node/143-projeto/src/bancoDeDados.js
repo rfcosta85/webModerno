cfc36034d0ac93236e89e4b9991b36b1d85e942d
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-27 17:41:08 -0300

    Aula 145- Projeto API com Express 03

--- 09-node/143-projeto/src/bancoDeDados.js
+++ 09-node/143-projeto/src/bancoDeDados.js
@@ -26,4 +26,13 @@ function getProdutos()
     return Object.values(produtos)
 }
 
-module.exports = { salvarProduto, getProduto, getProdutos }
+function excluirProduto(id)
+{
+
+    const produto = produtos[id]
+    delete produtos[id]
+
+    return produto
+}
+
+module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto }
--- 09-node/143-projeto/src/servidor.js
+++ 09-node/143-projeto/src/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003 //Porta significa um processo
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
 
@@ -19,11 +22,18 @@ app.post('/produtos', (req, res, next) => {
 
     const produto = bancoDeDados.salvarProduto({
 
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
 
     res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+
+    res.send(produto) //
 })
 
 app.listen(porta, () => {
