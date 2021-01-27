ecee31333578038e5ad6564fb9f6ad4b1214b1b0
Rodrigo Costa <rfcosta85@gmail.com>
2021-01-27 17:04:25 -0300

    Aula 144- Projeto API com Express 02

--- 09-node/143-projeto/src/servidor.js
+++ 09-node/143-projeto/src/servidor.js
@@ -2,11 +2,28 @@ const porta = 3003 //Porta significa um processo
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
 
-    res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON, via método send
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON, via método send
 
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+
+    const produto = bancoDeDados.salvarProduto({
+
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
