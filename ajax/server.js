8e32ecdb99056e51e5b0a5cc9160a8c3fbe82dbb
Rodrigo Costa <rfcosta85@gmail.com>
2021-03-13 18:53:40 -0300

    Aula 273- Implementando Server Js

---
+++ ajax/server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser') /* Será responsável por fazer um bodyParser do body da requisição, quando, você fizer um request e você mandar esses dados para o request, ele vai receber esses dados e deixar ele pronto para serem manipulados dentro do arquivo server.js */ 
+const express = require('express')
+const app = express()
+
+// MiddleWare 
+
+app.use(express.static('.')) // Ele vai prover dos arquivos estáticos, a partir da aplicação desse middleware 
+app.use(bodyParser.urlencoded({ extended: true })) // submit do formulário, vai ler os dados e transformar em objeto
+app.use(bodyParser.json()) // Aqui vai transformar o Json em objeto
+
+app.get('/teste', (req, res) => res.send('Ok')) 
+app.listen(8080, () => console.log('Executando...'))
