5121e9cbbf291a6bff530ac025beca1e6e1bf097
Rodrigo Costa <rfcosta85@gmail.com>
2021-03-15 16:40:54 -0300

    Aula 280- Axios 02

--- ajax/server.js
+++ ajax/server.js
@@ -38,5 +38,12 @@ app.post('/formulario', (req, res) => {
         id:1
     })
 })
+
+app.get('/parOuImpar', (req, res) => { 
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
  
 app.listen(8080, () => console.log('Executando...'))
