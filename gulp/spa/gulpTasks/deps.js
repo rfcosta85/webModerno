16707f9f45b8f126a1ba7e1980ba8cd8d71799d1
Rodrigo Costa <rfcosta85@gmail.com>
2021-03-21 14:41:23 -0300

    Aula 298- Criando a estrutuda do build

---
+++ gulp/spa/gulpTasks/deps.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+
+function depsCSS(cb) {
+
+    return cb()
+}
+
+function depsFonts(cb) {
+
+    return cb()
+}
+
+module.exports = { 
+    depsCSS,
+    depsFonts
+}
