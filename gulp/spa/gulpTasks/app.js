013253ba52803629c9246551490577091be916c8
Rodrigo Costa <rfcosta85@gmail.com>
2021-03-22 21:17:41 -0300

    Aula 301 Projeto SPA- Tasks Servidor

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,24 +1,46 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
-function appHTML(cb) {
+function appHTML() {
 
-    return cb()
+    return gulp.src('src/**/*.html') //minificar o html e jogar eles dentro da página build
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'))
 }
 
-function appCSS(cb) {
+function appCSS() {
 
-    return cb()
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function appJS(cb) {
+function appJS() {
 
-    return cb()
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({ presets: ['env'] }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
 
-function appIMG(cb) {
+function appIMG() {
 
-    return cb()
+    return gulp.src('src/assets/imgs/**/*.*')
+        .pipe(gulp.dest('build/assets/imgs'))
 }
+
+gulp.task('appHTML', appHTML)
+gulp.task('appCSS', appCSS)
+gulp.task('AppJS', appJS)
+gulp.task('appIMG', appIMG)
 
 module.exports = {
     appHTML,
--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp')
+const uglyfycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(cb) {
+function depsCSS() {
 
-    return cb()
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglyfycss({ "uglyComments": false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFonts(cb) {
+function depsFonts() {
 
-    return cb()
+    return gulp.src('node_modules/font-awesome/fonts/*.*') // vai pegar todas as fontes que estão dentro dessa página
+        .pipe(gulp.dest('build/assets/fonts')) // vai jogar para a pasta de destino
 }
 
 module.exports = { 
