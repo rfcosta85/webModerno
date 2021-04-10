4b90842e8f20bf73d39e95a4e1c2bb93b820cb7c
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-10 10:32:45 -0300

    Aula 363- Referenciando os Estilos SCSS

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,7 @@
+// Arquivos SASS
+
+// A Partir daqui o webpack monitora nossos arquivos e consegue importa todos eles para gerar o arquivo css final 
+
+// O index.js é o entry point, então é a partir dele que será achado o index.scss, o do bootstrap, font-a e o arquivo custom 
+
+import './scss/index.scss'
