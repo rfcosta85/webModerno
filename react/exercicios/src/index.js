1dfe0d47b1ff09d5773691afafcc1a6461bf7da2
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-19 20:20:42 -0300

    Aula 376- Componentes Pai e Filho 01

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 ReactDOM.render(
     <div>
 
-        <Saudacao tipo="Bom dia" nome="João"/>
+        <Pai nome="Paulo" sobrenome="Silva"/>
 
     </div>
 , document.getElementById('root'))
