5889ba2649d31aad2d08b674a051550bd2c9b77f
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-19 16:43:40 -0300

    Aula 371- Primeiro Componente

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React 2</h1>
+import Primeiro from './componentes/Primeiro'
 
-ReactDOM.render(elemento, document.getElementById('root'))
+
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
