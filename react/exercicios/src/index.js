cba2e03a48cfe41d78189592b20ffae41c7f3a68
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-19 17:09:11 -0300

    Aula 372- Componente com Parâmetro

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
-
-
-ReactDOM.render(<Primeiro />, document.getElementById('root'))
+ReactDOM.render(<BomDia nome="Bom dia Guilherme"/>, document.getElementById('root'))
