716370efa615b7393c6fc729cfd670b8456d503e
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-20 15:51:36 -0300

    Aula 379- Componente Calculator

---
+++ react/calculadora/src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+
+                
+
+            </div>
+        )
+    }
+}
