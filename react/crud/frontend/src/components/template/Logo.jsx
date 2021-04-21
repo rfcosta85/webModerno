1a723c82dfe6039ee86b1c1d1651160c1431a2ba
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-21 16:23:23 -0300

    Aula 389- Componente Cabeçalho

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 /*Definindo que todos os componentes serão funcionais*/ 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+        </a>
     </aside>
