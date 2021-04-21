30a4735bfe5a90dfa484fcc60c052bd9d49d4dd6
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-21 12:08:53 -0300

    Aula 384- Implementando a logica 02

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -4,9 +4,7 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
-//Definindo o estado inicial da calculadora 
-
-const initialState = { 
+const initialState = {
     displayValue: '0',
     clearDisplay: false,
     operation: null,
@@ -16,7 +14,7 @@ const initialState = {
 
 export default class Calculator extends Component {
 
-    state = {...initialState}
+    state = { ...initialState }
 
     constructor(props) {
         super(props)
@@ -27,55 +25,78 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        this.setState({...initialState})
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true }) // Aqui realiza a captura dos valores binários e no final ele irá efetuar o resultado
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation // Ela pega a operação que ja foi realizada ex quando somamos mais de um número
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
-    
+
     addDigit(n) {
-        if(n==="." && this.state.displayValue.includes('.')){
+        if (n === '.' && this.state.displayValue.includes('.')) {
             return
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
-        const displayValue = currentValue + n 
-        this.setState({displayValue, clearDisplay: false})
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
 
-        if(n !== '.') {
-            const i = this.state.current 
+        if (n !== '.') {
+            const i = this.state.current
             const newValue = parseFloat(displayValue)
             const values = [...this.state.values]
             values[i] = newValue
-            this.setState = ({values})
+            this.setState({ values })
             console.log(values)
         }
     }
-    render() {        
+
+    render() {
         return (
             <div className="calculator">
-
-                <Display value={this.state.displayValue}/>
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
-                <Button label="/"click={this.setOperation} operation/>
-                <Button label="7" click={this.addDigit}/>
-                <Button label="8" click={this.addDigit}/>
-                <Button label="9" click={this.addDigit}/>
-                <Button label="*" click={this.setOperation} operation/>
-                <Button label="4" click={this.addDigit}/>
-                <Button label="5" click={this.addDigit}/>
-                <Button label="6" click={this.addDigit}/>
-                <Button label="-" click={this.setOperation} operation/>
-                <Button label="1" click={this.addDigit}/>
-                <Button label="2" click={this.addDigit}/>
-                <Button label="3" click={this.addDigit}/>
-                <Button label="+" click={this.setOperation} operation/>
-                <Button label="0" click={this.addDigit} double/>
-                <Button label="." click={this.addDigit}/>  
-                <Button label="=" click={this.setOperation} operation/>              
-
+                <Button label="/" click={this.setOperation} operation />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} operation />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} operation />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation />
+                <Button label="0" click={this.addDigit} double />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation />
+                
             </div>
         )
     }
